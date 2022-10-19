d18d53790a81bbaf99a9d1693fa5ad6f734f6eb7
AlexRTC1997 <aturpoco@unsa.edu.pe>
2022-10-19 12:16:20 -0500

    ADD: Go to Menu Activity from Login Activity with Intent (Version 1.0)

--- app/src/main/java/com/example/laboratorio03/LoginActivity.java
+++ app/src/main/java/com/example/laboratorio03/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.laboratorio03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /* TODO: Update code
+        * Go to MenuActivity (old version)
+        * Without username and password validation
+    */
+    public void goToMenuActivity(View view) {
+        Intent menuIntent = new Intent(this, MenuActivity.class);
+        startActivity(menuIntent);
     }
 }
