d0aa10a438cfaa35559311bfab821d21c2469925
bgomezv123 <bgomezv@unsa.edu.pe>
2022-10-22 23:42:03 -0500

    Cambios hechos

--- app/src/main/java/com/example/laboratorio03/PostulantRegisterActivity.java
+++ app/src/main/java/com/example/laboratorio03/PostulantRegisterActivity.java
@@ -2,13 +2,32 @@ package com.example.laboratorio03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class PostulantRegisterActivity extends AppCompatActivity {
 
+    EditText dni, nombres, apellidos , fecha, colegio , carrera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postulant_register);
     }
+
+    public void crearPostulante(){
+        dni = findViewById(R.id.dniRegistro);
+        nombres = findViewById(R.id.nombresRegistro);
+        apellidos = findViewById(R.id.apellidosRegistro);
+        fecha = findViewById(R.id.dateRegistro);
+        colegio = findViewById(R.id.colegioRegistro);
+        carrera = findViewById(R.id.carreraRegistro);
+
+        System.out.println("DNI");
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/laboratorio03/Postulante.java
@@ -0,0 +1,20 @@
+package com.example.laboratorio03;
+
+public class Postulante {
+    String dni;
+    String nombres;
+    String apellidos;
+    String fecha;
+    String colegio;
+    String carrera;
+
+
+    public Postulante(String _dni, String _nombres, String _apellidos, String _fecha, String _colegio, String _carrera  ){
+        dni = _dni;
+        nombres = _nombres;
+        apellidos = _apellidos;
+        fecha = _fecha;
+        colegio = _colegio;
+        carrera = _carrera;
+    }
+}
