42aa7f0b5d115f3e98afdf882ee2c8b0e9195865
JoseAntonio98 <sawm5798@gmail.com>
2022-11-03 10:29:54 -0500

    UPDATED imports

--- app/src/main/java/com/example/laboratorio03/PostulantInfoActivity.java
+++ app/src/main/java/com/example/laboratorio03/PostulantInfoActivity.java
@@ -3,14 +3,11 @@ package com.example.laboratorio03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
