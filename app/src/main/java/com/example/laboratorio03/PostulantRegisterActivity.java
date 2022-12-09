d141a50e0c2dd0a4bb9e495cc8c775db0052c767
bgomezv123 <bgomezv@unsa.edu.pe>
2022-12-08 20:42:14 -0500

    Se logran guardar los datos en un texto plano en la app specific storage

--- app/src/main/java/com/example/laboratorio03/PostulantRegisterActivity.java
+++ app/src/main/java/com/example/laboratorio03/PostulantRegisterActivity.java
@@ -2,12 +2,24 @@ package com.example.laboratorio03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 
 
 public class PostulantRegisterActivity extends AppCompatActivity {
@@ -15,11 +27,13 @@ public class PostulantRegisterActivity extends AppCompatActivity {
     EditText dni, nombres, apellidos , fecha, colegio , carrera;
     Button btnRegister;
     Postulante p1;
+    Context context;
+    private static final String FILE_NAME = "postulante_file";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postulant_register);
-
+        context= getApplicationContext();
         btnRegister = findViewById(R.id.botonRegistrar);
         dni = findViewById(R.id.dniRegistro);
         nombres = findViewById(R.id.nombresRegistro);
@@ -45,17 +59,53 @@ public class PostulantRegisterActivity extends AppCompatActivity {
                     Toast.makeText(PostulantRegisterActivity.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
                     Intent data = new Intent(PostulantRegisterActivity.this, MenuActivity.class);
                     data.putExtra("pNew", p1);
+
+                    WritePostulante(dniT,apeM,nomb,fchN,colg,carr);
                     setResult(RESULT_OK, data);
                     finish();
                 }
                 else {
                     Toast.makeText(PostulantRegisterActivity.this, "Verifique los datos puestos", Toast.LENGTH_SHORT).show();
-                }
 
+                }
             }
         });
+    }
 
-
+    public void WritePostulante(String dni,String nombres, String apellidos, String fecha, String colegio , String carrera ){
+        try(FileOutputStream fos = context.openFileOutput(FILE_NAME, MODE_APPEND)){
+            fos.write((dni+"\n").getBytes());
+            fos.write((nombres+"\n").getBytes());
+            fos.write((apellidos+"\n").getBytes());
+            fos.write((fecha+"\n").getBytes());
+            fos.write((colegio+"\n").getBytes());
+            fos.write((carrera+"\n").getBytes());
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e ){
+            e.printStackTrace();
+        }
+    }
+    public String ReadPostulante(){
+        StringBuilder sb = new StringBuilder();
+        String userName = "";
+        try {
+            FileInputStream fis = context.openFileInput(FILE_NAME);
+            InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(inputStreamReader);
+            String line = br.readLine();
+            while(line != null){
+                sb.append(line);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch ( IOException e){
+            e.printStackTrace();
+        } finally {
+            userName = sb.toString();
+        }
+        return userName.replace("\n","").replace("\r","");
     }
 
 }
